045f5a55bacc1c32ae5250d497f9fc38b72b0b0f
Jil <upadhyay.jil1802@gmail.com>
2025-01-18 18:22:50 +0530

    added user info and icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Avatar, Box } from '@mui/material';
+import { 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  IconButton, 
+  Menu, 
+  MenuItem, 
+  Avatar, 
+  Box,
+  Divider,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signOut } from 'firebase/auth';
 
@@ -10,7 +24,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
   const navigate = useNavigate();
   const auth = getAuth();
   const user = auth.currentUser;
-  const firstLetter = user?.displayName?.charAt(0).toUpperCase() || 'U'; // Get first letter of the name
+  const firstLetter = user?.displayName?.charAt(0).toUpperCase() || 'U';
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -44,9 +58,43 @@ const Navbar = ({ darkMode, setDarkMode }) => {
         <IconButton onClick={handleMenuOpen} sx={{ ml: 2 }}>
           <Avatar sx={{ bgcolor: darkMode ? '#ff5722' : '#4caf50' }}>{firstLetter}</Avatar>
         </IconButton>
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-          <MenuItem onClick={handleProfileSettings}>Profile Settings</MenuItem>
-          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        <Menu 
+          anchorEl={anchorEl} 
+          open={Boolean(anchorEl)} 
+          onClose={handleMenuClose}
+          PaperProps={{
+            sx: {
+              width: '250px',
+              mt: 1.5
+            }
+          }}
+        >
+          {/* User Info Section */}
+          <Box sx={{ px: 2, py: 1.5 }}>
+            <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>
+              {user?.displayName || 'User'}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {user?.email || 'No email'}
+            </Typography>
+          </Box>
+          
+          <Divider />
+          
+          {/* Menu Items */}
+          <MenuItem onClick={handleProfileSettings}>
+            <ListItemIcon>
+              <AccountCircleIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Profile Settings</ListItemText>
+          </MenuItem>
+          
+          <MenuItem onClick={handleLogout}>
+            <ListItemIcon>
+              <LogoutIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Logout</ListItemText>
+          </MenuItem>
         </Menu>
       </Toolbar>
     </AppBar>
